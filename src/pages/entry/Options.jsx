0b1c7605a12b61c9d7e5b3944eb0b1368ee089f2
Abhishek <abhi101993@gmail.com>
2021-05-16 01:39:15 +0530

    destructure props in options.tsx

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import ScoopOption from './ScoopOption';
+import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
+import ScoopOption from './ScoopOption';
 
-const Options = (optionType) => {
+const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:3030/${optionType}`)
-      .then((response) => setItems(response.data))
-      .catch((error) => {});
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`http://localhost:3030/${optionType}`);
+
+        setItems(res.data);
+      } catch (error) {}
+    };
+
+    fetchData();
   }, [optionType]);
 
+  // TODO: replace `null` with ToppingOption when available
   const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
 
   const optionItems = items.map((item) => (
