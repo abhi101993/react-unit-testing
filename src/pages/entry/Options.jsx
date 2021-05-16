d0e3d5eb6bacab93e50de8d21a80c1bf2a23393b
Abhishek <abhi101993@gmail.com>
2021-05-16 14:47:10 +0530

    Test and code for alert for server error response

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -2,9 +2,13 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
+import AlertBanner from '../common/AlertBanner';
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
+
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,14 +16,20 @@ const Options = ({ optionType }) => {
         const res = await axios.get(`http://localhost:3030/${optionType}`);
 
         setItems(res.data);
-      } catch (error) {}
+      } catch (error) {
+        setError(true);
+      }
     };
 
     fetchData();
   }, [optionType]);
 
+  if (error) {
+    return <AlertBanner />;
+  }
+
   // TODO: replace `null` with ToppingOption when available
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
     <ItemComponent
